2c4aee173206a7ce8e30cdb154c85dd47a7bfc2d
Bin Nguyen <nguyen.huu.vinh@moneyforward.vn>
2019-05-14 09:46:31 +0700

    layout for add button

---
+++ IoTGarden/UIColor+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Extension.swift
+//  IoTGarden
+//
+//  Created by Vinh Nguyen on 5/13/19.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var bgTapped: UIColor {
+        
+        return UIColor(red: 230.0 / 255.0, green: 231.0 / 255.0, blue: 235.0 / 255.0, alpha: 1.0)
+    }
+}
